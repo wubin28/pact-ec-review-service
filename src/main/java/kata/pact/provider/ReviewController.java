c23b1fbb6aec87acf80fbed3e937ee7076e2b1c9
Ben Wu <wubin28@gmail.com>
2017-11-22 13:28:38 +0800

    get parameters from pact

--- src/main/java/kata/pact/provider/ReviewController.java
+++ src/main/java/kata/pact/provider/ReviewController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @Controller
 public class ReviewController {
     private ReviewService myResponseService;
 
     @RequestMapping(value = "/ratings", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public ResponseEntity<List<Rating>> getRatings() {
+    public ResponseEntity<List<Rating>> getRatings(String id,String name) {
         ReviewService reviewService = new ReviewService();
+        assertEquals("123", id);
+        assertEquals("ben", name);
         return reviewService.getRatings();
     }
 
