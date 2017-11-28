7d3116a78f94f56264ebafcd571f3f17071c6fc9
Ben Wu <wubin28@gmail.com>
2017-11-28 10:12:40 +0800

    implement pact provider using pact-jvm-provider-junit_2.11

--- src/main/java/kata/pact/provider/ReviewService.java
+++ src/main/java/kata/pact/provider/ReviewService.java
@@ -1,17 +1,14 @@
 package kata.pact.provider;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewService {
-    public ResponseEntity<List<Rating>> getRatings(String id, String name) {
+    public List<Rating> getRatings(String id, String name) {
         // use id and name to get the ratings from the backend
 
         List<Rating> result = new ArrayList<Rating>();
         result.add(new Rating("345678", "ben", "1"));
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return result;
     }
 }
